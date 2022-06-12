2157d3c3a0d2c61e76874411f1c2b05eb1efb07b
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-06-12 22:23:00 +0300

    emplemented services for fighters

--- services/fighterService.js
+++ services/fighterService.js
@@ -2,6 +2,27 @@ const { FighterRepository } = require('../repositories/fighterRepository');
 
 class FighterService {
     // TODO: Implement methods to work with fighters
+
+    getFighter() {          //GET /api/fighters
+        return FighterRepository.getAll();
+       };
+ 
+       getFighterById(id) {          //GET /api/fighters/:id
+         return FighterRepository.getOne(id);
+        };
+ 
+        updateFighterData(id, data) {               //POST /api/fighters
+         return FighterRepository.update(id, data);
+        };
+ 
+        createFighter(userData) {                   //PUT /api/fighters/:id
+         return FighterRepository.create(userData);
+        };
+ 
+        deleteFighter(id) {                         //DELETE /api/fighters/:id
+         return FighterRepository.delete(id);
+        };
+ 
 }
 
 module.exports = new FighterService();
