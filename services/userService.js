83890afbfa95f36ecf8b34233bc6df758796e5c9
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-06-12 20:55:00 +0300

    emplemented services for users

--- services/userService.js
+++ services/userService.js
@@ -4,6 +4,27 @@ class UserService {
 
     // TODO: Implement methods to work with user
 
+    getUsers() {          //GET /api/users
+       return UserRepository.getAll();
+      };
+
+      getUserById(id) {          //GET /api/users/:id
+        return UserRepository.getOne(id);
+       };
+
+       updateUserData(id, data) {               //POST /api/users
+        return UserRepository.update(id, data);
+       };
+
+       createUser(userData) {                   //PUT /api/users/:id
+        return UserRepository.create(userData);
+       };
+
+       deleteUser(id) {                         //DELETE /api/users/:id
+        return UserRepository.delete(id);
+       };
+
+
     search(search) {
         const item = UserRepository.getOne(search);
         if(!item) {
