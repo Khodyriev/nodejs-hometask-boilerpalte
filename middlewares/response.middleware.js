99143365675ff06dbe358457e697cb3c6341814c
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-06-13 18:27:33 +0300

    response middleware saved

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,20 @@
 const responseMiddleware = (req, res, next) => {
    // TODO: Implement middleware that returns result of the query
+
+   if (res.statusCode === 200 && res.data) {
+    return res.status(res.statusCode).json(res.data);
+}
+if (res.err) {
+    return res
+        .status(res.statusCode)
+        .json({ error: true, message: `${res.err}` });
+}
+if (res.statusCode) {
+    return res.status(404).json({
+        error: true,
+        message: "Requested data not found",
+    });
+}
     next();
 }
 
