840ddbb9a02dc1a5d1fde6f2b9a4ef97c5db2d9e
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-06-13 18:26:45 +0300

    added routers logic

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,19 @@
 const { Router } = require('express');
 const AuthService = require('../services/authService');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+const { responseMiddleware: middleware } = require('../middlewares/response.middleware');
+
 
 const router = Router();
 
 router.post('/login', (req, res, next) => {
     try {
         // TODO: Implement login action (get the user if it exist with entered credentials)
+        const { email, password } = req.body;
+        if (email) {
+            res.data = AuthService.login({ email, password });
+        } else {
+            throw Error("Invalid user authorisation!");
+        }
         res.data = data;
     } catch (err) {
         res.err = err;
